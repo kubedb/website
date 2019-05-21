9f193717963e9b45b5ef2a416ae12cbe97b56876
mohincse <mohin@appscode.com>
2019-05-21 17:18:39 +0600

    New style add in table and image and fixing right sidebar css

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,117 +1,126 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
     $navbarBurgers.forEach(el => {
-      el.addEventListener('click', () => {
-
+      el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   }
 });
 
-
-
 // menu sticky
 // Not a ton of code, but hard to
-const nav = document.querySelector('#header');
+const nav = document.querySelector("#header");
 let topOfNav = nav.offsetTop;
 
 function fixNav() {
   if (window.scrollY > topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('fixed-nav');
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
   } else {
-    document.body.classList.remove('fixed-nav');
+    document.body.classList.remove("fixed-nav");
     document.body.style.paddingTop = 0;
   }
 }
 
-window.addEventListener('scroll', fixNav);
-
-
-
+window.addEventListener("scroll", fixNav);
 
 //bulma carousel
-bulmaCarousel.attach('#carousel-demo', {
+bulmaCarousel.attach("#carousel-demo", {
   slidesToScroll: 1,
   slidesToShow: 1,
   infinite: true,
   autoplay: false
-
 });
 
-
 // For FAQ Collaps Page
-const accordionItem = document.querySelectorAll('.accordion-item');
-
+const accordionItem = document.querySelectorAll(".accordion-item");
 const onClickAccordionHeader = e => {
-  if (e.currentTarget.parentNode.classList.contains('active')) {
+  if (e.currentTarget.parentNode.classList.contains("active")) {
     e.currentTarget.parentNode.classList.remove("active");
   } else {
-    Array.prototype.forEach.call(accordionItem, (e) => {
-      e.classList.remove('active');
+    Array.prototype.forEach.call(accordionItem, e => {
+      e.classList.remove("active");
     });
     e.currentTarget.parentNode.classList.add("active");
   }
 };
-
 const init = () => {
-  Array.prototype.forEach.call(accordionItem, (e) => {
-    e.querySelector('.accordion-header').addEventListener('click', onClickAccordionHeader, false);
+  Array.prototype.forEach.call(accordionItem, e => {
+    e.querySelector(".accordion-header").addEventListener(
+      "click",
+      onClickAccordionHeader,
+      false
+    );
   });
 };
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
-
-
-
+// Right sidebar SpyScrolling
 const makeNavLinksSmooth = () => {
-  const navLinks = document.querySelectorAll('.nav-link');
+  const navLinks = document.querySelectorAll(".nav-link");
 
   for (let n in navLinks) {
     if (navLinks.hasOwnProperty(n)) {
-      navLinks[n].addEventListener('click', e => {
+      navLinks[n].addEventListener("click", e => {
         e.preventDefault();
         const el = document.querySelector(navLinks[n].hash);
         const pos = el.offsetTop;
         window.scrollTo({
           top: pos,
-          behavior: 'smooth'
+          behavior: "smooth"
         });
       });
     }
   }
-}
-
+};
 const spyScrolling = () => {
-  const sections = document.querySelectorAll('.single-info');
+  const sections = document.querySelectorAll(".single-info");
 
   window.onscroll = () => {
-    const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollPos =
+      document.documentElement.scrollTop || document.body.scrollTop;
 
     for (let s in sections)
       if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
         const id = sections[s].id;
-        document.querySelector('.active').classList.remove('active');
-        document.querySelector(`a[href*=${ id }]`).parentNode.classList.add('active');
+        document.querySelector(".active").classList.remove("active");
+        document
+          .querySelector(`a[href*=${id}]`)
+          .parentNode.classList.add("active");
       }
-  }
-}
+  };
+};
 
 makeNavLinksSmooth();
 spyScrolling();
 
+// tabs active class add script
+//var tabPane = document.querySelector(".tab-pane");
+var btns = document.querySelectorAll(".nav-link");
+var sbtn = Array.prototype.slice.call(btns);
+sbtn.forEach(el => {
+  el.addEventListener("click", function() {
+    Object.values(btns).forEach(btn => {
+      if (btn !== el) {
+        btn.classList.remove("active");
+      }
+    });
+    el.classList.toggle("active");
+  });
+});
