287ac115d61c70c10b28e957ad3f1cd3c5b0ed82
MD MOHIN <45921824+csemohin@users.noreply.github.com>
2019-05-24 23:17:14 +0600

    Add new style in table, tabs and docs content. (#2)
    * fix docs page style
    * New style add in table and image and fixing right sidebar css
    * Fix header button size and reduce spacing header to full content
    * Get tabs-selection (script/helm) dynamic in setup page
    * Go the the section smoothly when click on a table-of-content item
    * add .active dynamically to TOC

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,117 +1,150 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
-
     // Add a click event on each of them
     $navbarBurgers.forEach(el => {
-      el.addEventListener('click', () => {
-
+      el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   }
 });
 
-
-
 // menu sticky
 // Not a ton of code, but hard to
-const nav = document.querySelector('#header');
+const nav = document.querySelector("#header");
 let topOfNav = nav.offsetTop;
 
 function fixNav() {
   if (window.scrollY > topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
-    document.body.classList.add('fixed-nav');
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
   } else {
-    document.body.classList.remove('fixed-nav');
+    document.body.classList.remove("fixed-nav");
     document.body.style.paddingTop = 0;
   }
 }
 
-window.addEventListener('scroll', fixNav);
-
-
-
+window.addEventListener("scroll", fixNav);
 
 //bulma carousel
-bulmaCarousel.attach('#carousel-demo', {
+bulmaCarousel.attach("#carousel-demo", {
   slidesToScroll: 1,
   slidesToShow: 1,
   infinite: true,
   autoplay: false
-
 });
 
-
 // For FAQ Collaps Page
-const accordionItem = document.querySelectorAll('.accordion-item');
-
+const accordionItem = document.querySelectorAll(".accordion-item");
 const onClickAccordionHeader = e => {
-  if (e.currentTarget.parentNode.classList.contains('active')) {
+  if (e.currentTarget.parentNode.classList.contains("active")) {
     e.currentTarget.parentNode.classList.remove("active");
   } else {
-    Array.prototype.forEach.call(accordionItem, (e) => {
-      e.classList.remove('active');
+    Array.prototype.forEach.call(accordionItem, e => {
+      e.classList.remove("active");
     });
     e.currentTarget.parentNode.classList.add("active");
   }
 };
-
 const init = () => {
-  Array.prototype.forEach.call(accordionItem, (e) => {
-    e.querySelector('.accordion-header').addEventListener('click', onClickAccordionHeader, false);
+  Array.prototype.forEach.call(accordionItem, e => {
+    e.querySelector(".accordion-header").addEventListener(
+      "click",
+      onClickAccordionHeader,
+      false
+    );
   });
 };
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
-
-
-
-const makeNavLinksSmooth = () => {
-  const navLinks = document.querySelectorAll('.nav-link');
-
-  for (let n in navLinks) {
-    if (navLinks.hasOwnProperty(n)) {
-      navLinks[n].addEventListener('click', e => {
-        e.preventDefault();
-        const el = document.querySelector(navLinks[n].hash);
-        const pos = el.offsetTop;
-        window.scrollTo({
-          top: pos,
-          behavior: 'smooth'
-        });
+// Table Of Content
+// go the the section smoothly when click on a table-of-content item
+const goToASectionSmoothly = () => {
+  const tocItems = document.querySelectorAll("#TableOfContents a");
+  tocItems.forEach(item => {
+    item.addEventListener("click", e => {
+      e.preventDefault();
+      // go to the target section smoothly
+      const targetEl = document.querySelector(e.currentTarget.hash);
+      const pos = targetEl.offsetTop;
+      window.scrollTo({
+        top: pos,
+        behavior: "smooth"
       });
-    }
-  }
-}
+    });
+  });
+};
 
+// add .active dynamically to TOC
 const spyScrolling = () => {
-  const sections = document.querySelectorAll('.single-info');
+  const allHeaders = document.querySelectorAll("h1, h2, h3, h4");
 
   window.onscroll = () => {
-    const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-
-    for (let s in sections)
-      if (sections.hasOwnProperty(s) && sections[s].offsetTop <= scrollPos) {
-        const id = sections[s].id;
-        document.querySelector('.active').classList.remove('active');
-        document.querySelector(`a[href*=${ id }]`).parentNode.classList.add('active');
+    const scrollPos =
+      document.documentElement.scrollTop || document.body.scrollTop;
+    for (let s in allHeaders) {
+      if (
+        allHeaders.hasOwnProperty(s) &&
+        allHeaders[s].offsetTop <= scrollPos
+      ) {
+        const id = allHeaders[s].id;
+        if (id) {
+          document.querySelectorAll("#TableOfContents a").forEach(a => {
+            console.log("id: ", id);
+            if (`#${id}` === a.hash) {
+              a.classList.add("active");
+            } else {
+              a.classList.remove("active");
+            }
+          });
+        }
+        
       }
-  }
-}
+    }
+  };
+};
 
-makeNavLinksSmooth();
+goToASectionSmoothly();
 spyScrolling();
 
+// tabs active class add script - setup | install page
+const tabItems = document.querySelectorAll(".nav-item .nav-link");
+tabItems.forEach(tab => {
+  tab.addEventListener("click", e => {
+    e.preventDefault();
+    const el = e.currentTarget;
+
+    // add .active class to the clicked item, remove .active from others
+    document.querySelectorAll(".nav-item .nav-link").forEach(navLink => {
+      navLink === el
+        ? navLink.classList.add("active")
+        : navLink.classList.remove("active");
+    });
+
+    // add .show class to the target tab-pane, remove from others
+    const elHref = el.getAttribute("href");
+    const tabPaneTarget = document.querySelector(elHref);
+
+    document.querySelectorAll(".tab-pane").forEach(tabPane => {
+      tabPane === tabPaneTarget
+        ? tabPane.classList.add("show")
+        : tabPane.classList.remove("show");
+    });
+
+    tabPane.classList.add("show");
+  });
+});
