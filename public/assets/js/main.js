aecaae44a0304a16e6ae1b9a5f71dc7ccab64413
Tamal Saha <tamal@appscode.com>
2019-05-28 19:30:07 -0700

    Update public folder

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -22,22 +22,31 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+
 // menu sticky
 // Not a ton of code, but hard to
-const nav = document.querySelector("#header");
+const nav = document.querySelector('#header, .kubedb-documentation-menu');
 let topOfNav = nav.offsetTop;
 
 function fixNav() {
   if (window.scrollY > topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + "px";
-    document.body.classList.add("fixed-nav");
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
   } else {
-    document.body.classList.remove("fixed-nav");
+    document.body.classList.remove('fixed-nav');
     document.body.style.paddingTop = 0;
   }
 }
 
-window.addEventListener("scroll", fixNav);
+window.addEventListener('scroll', fixNav);
+
+// kd version dropdown js
+var kdDropdown = document.querySelector(".dropdown");
+var kdDropdownBtn = document.querySelector(".dropdown-trigger");
+kdDropdownBtn.addEventListener("click", function(){
+  kdDropdown.classList.toggle("is-active");
+});
+
 
 //bulma carousel
 bulmaCarousel.attach("#carousel-demo", {
