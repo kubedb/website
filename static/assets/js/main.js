a1b214627fc09558f04369b5c7b77f11551ef7ba
mohincse <mohin@appscode.com>
2019-08-07 18:43:33 +0600

    Add responsive code, fix header js, update feature section

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".navbar-burger"),
     0
   );
-
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
     // Add a click event on each of them
@@ -13,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target;
         const $target = document.getElementById(target);
-
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle("is-active");
         $target.classList.toggle("is-active");
@@ -22,12 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-
 // menu sticky
 // Not a ton of code, but hard to
 const nav = document.querySelector('#header, .kubedb-documentation-menu');
 let topOfNav = nav.offsetTop;
-
 function fixNav() {
   if (window.scrollY > topOfNav) {
     document.body.style.paddingTop = nav.offsetHeight + 'px';
@@ -37,11 +33,22 @@ function fixNav() {
     document.body.style.paddingTop = 0;
   }
 }
-
 window.addEventListener('scroll', fixNav);
 
-
-
+// mega menu active class
+var navbarItems = document.querySelectorAll(".navbar-item");
+navbarItems.forEach(navbarItem => {
+  navbarItem.addEventListener("click", function() {
+    var megamenues = document.querySelectorAll(".navbar-item > .ac-megamenu , .navbar-item > .ac-dropdown");
+    // remove is-active class from all the megamenus except the navbar item that was clicked
+    megamenues.forEach(megamenu => {
+      // toggle classes
+      if (megamenu.parentElement === navbarItem)
+        megamenu.classList.toggle("is-active");
+      else megamenu.classList.remove("is-active");
+    });
+  });
+});
 
 //bulma carousel
 bulmaCarousel.attach("#carousel-demo", {
@@ -72,7 +79,6 @@ const init = () => {
     );
   });
 };
-
 document.addEventListener("DOMContentLoaded", init);
 
 // Table Of Content
@@ -152,3 +158,33 @@ tabItems.forEach(tab => {
     tabPane.classList.add("show");
   });
 });
+
+
+// scroll to top
+var basicScrollTop = function() {
+  // The button
+  var btnTop = document.querySelector("#goTop");
+  if (btnTop) {
+    // Reveal the button
+    var btnReveal = function() {
+      if (window.scrollY >= 300) {
+        btnTop.classList.add("is-visible");
+      } else {
+        btnTop.classList.remove("is-visible");
+      }
+    };
+    // Smooth scroll top
+    var TopscrollTo = function() {
+      if (window.scrollY != 0) {
+        setTimeout(function() {
+          window.scrollTo(0, window.scrollY - 30);
+          TopscrollTo();
+        }, 5);
+      }
+    };
+    // Listeners
+    window.addEventListener("scroll", btnReveal);
+    btnTop.addEventListener("click", TopscrollTo); 
+  }
+};
+basicScrollTop();
