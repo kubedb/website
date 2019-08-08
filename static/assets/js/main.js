16084b3ad85fc140a1b4e2dfdb490013cce69f46
mohincse <mohin@appscode.com>
2019-08-08 09:59:08 +0600

    Remove button focus border

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -26,7 +26,6 @@ const nav = document.querySelector('#header, .kubedb-documentation-menu');
 let topOfNav = nav.offsetTop;
 function fixNav() {
   if (window.scrollY > topOfNav) {
-    document.body.style.paddingTop = nav.offsetHeight + 'px';
     document.body.classList.add('fixed-nav');
   } else {
     document.body.classList.remove('fixed-nav');
